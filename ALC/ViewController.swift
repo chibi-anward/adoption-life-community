d2b509465ca18514b8488451769ef785525123ea
CADigitalArt <info@chibianward.com>
2017-07-01 15:14:07 +0200

    Initial Commit

---
+++ ALC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALC
+//
+//  Created by Chibi Anward on 2017-07-01.
+//  Copyright © 2017 chibi.anward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
